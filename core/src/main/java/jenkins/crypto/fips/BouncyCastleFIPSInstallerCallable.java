170d1087600a675646dfd0be3f077dd7cd6fbfb5
James Nord <jtnord@users.noreply.github.com>
2021-06-24 15:41:43 +0100

    [BEE-6127] configure agents as they connect

---
+++ core/src/main/java/jenkins/crypto/fips/BouncyCastleFIPSInstallerCallable.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.crypto.fips;
+
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import hudson.remoting.Channel;
+import hudson.slaves.SlaveComputer;
+import jenkins.security.MasterToSlaveCallable;
+
+/**
+ * Allows registering Bouncy Castle on a remote agent. Just call {@link #on(Channel)} or {@link #on(SlaveComputer)}.
+ *
+ */
+@Restricted(NoExternalUse.class)
+class BouncyCastleFIPSInstallerCallable extends MasterToSlaveCallable<Void, Exception> {
+
+    private static final long serialVersionUID = 1L;
+
+    private final boolean fipsOnlyAlgorithms; 
+
+    BouncyCastleFIPSInstallerCallable() {
+        this.fipsOnlyAlgorithms = BouncyCastleFIPSIntaller.isApprovedOnlyMode();
+    }
+
+    @Override
+    public Void call() throws Exception {
+        BouncyCastleFIPSIntaller.install(fipsOnlyAlgorithms);
+        return null;
+    }
+
+}
---
+++ core/src/main/java/jenkins/crypto/fips/BouncyCastleFIPSInstallerComputerListener.java
@@ -0,0 +1,44 @@
+package jenkins.crypto.fips;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.Computer;
+import hudson.model.TaskListener;
+import hudson.remoting.Channel;
+import hudson.slaves.ComputerListener;
+
+/**
+ * {@link ComputerListener} that registers BouncyCastle on new Agents as they connect.
+ */
+@Restricted(NoExternalUse.class)
+@Extension
+public class BouncyCastleFIPSInstallerComputerListener extends ComputerListener {
+
+    private static final Logger LOG = Logger.getLogger(BouncyCastleFIPSInstallerComputerListener.class.getName());
+
+
+    @Override
+    public void preOnline(Computer c, Channel channel, FilePath root, TaskListener listener)
+            throws IOException, InterruptedException {
+        try {
+            // we should not need to preload the jars as the installer class instantiated ovjects from them so they should appear magically.
+            //channel.preloadJar(BouncyCastleFipsProvider.class.getClassLoader(), BouncyCastleFipsProvider.class); // bc-fips
+            //channel.preloadJar(BouncyCastleJsseProvider.class.getClassLoader(), BouncyCastleJsseProvider.class); // bc-tls
+
+            channel.call(new BouncyCastleFIPSInstallerCallable());
+        } catch (Exception e) {
+            listener.error("Failed to configure java security extensions, see system logs for more details.");
+            LOG.log(Level.WARNING, "Failed to configure java security extensions on " + c.getName(), e);
+            if (BouncyCastleFIPSIntaller.isApprovedOnlyMode()) {
+                // the agent is not FIPS compliant, throw the exception to prevent it being used.
+                throw new IOException("Could not configure the computer for FIPS compliance", e);
+            }
+        }
+    }
+}
+
